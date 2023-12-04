e6818c545df2de8544687f4724c7118c0792b301
Erast Koliasa <erast.koliasa@gmail.com>
2023-12-04 17:31:28 +0200

    Created the User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,28 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-
+    firstName: {type: String, required: true, minlength: 4, maxlength: 50,  trim: true},
+    lastName: {type: String, required: true, minlength: 3, maxlength: 60, trim: true},
+    fullName: String,
+    email: {type: String, required: true, unique: true, lowercase: true},
+    role: {type: String, enum: ['admin', 'writer', 'guest']},
+    age: {type: Number, min: 1, max: 99, default: 1},
+    numberOfArticles: {type: Number, default: 0},
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: Date,
 });
+
+userSchema.pre('save', function (next) {
+    this.fullName = `${this.firstName} ${this.lastName}`;
+    next();
+  });
+  
+  userSchema.pre('save', function (next) {
+    if (this.age < 0) {
+      this.age = 1;
+    }
+    next();
+  });
 
 const User = mongoose.model('User', userSchema);
 
